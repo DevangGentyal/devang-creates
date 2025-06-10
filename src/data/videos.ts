3403d6932f8423081b90bf6cdd7e41acffbc3de2
Devang Gentyal <132248630+DevangGentyal@users.noreply.github.com>
2025-06-11 00:26:31 +0530

    Update videos.ts

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -3,74 +3,89 @@ import { VideoType } from '../types';
 export const videos: VideoType[] = [
   {
     id: '1',
+    title: 'Best Vlog Edits',
+    category: 'vlog',
+    type: 'long',
+    youtubeId: 'VcNNCR__HI0'
+  },
+  {
+    id: '2',
     title: 'College Character Day Gets Wild 😭',
     category: 'vlog',
     type: 'long',
     youtubeId: '0mP8VPjmzlg'
   },
   {
-    id: '2',
+    id: '3',
     title: 'Snoop Dogg in Olympics 2024?',
     category: 'documentary',
     type: 'long',
     youtubeId: 'i57px8pzzWI'
   },
   {
-    id: '3',
+    id: '4',
     title: 'How To Become A Millionaire in 2024',
     category: 'documentary',
     type: 'long',
     youtubeId: 'fupZr88o7-M'
   },
   {
-    id: '4',
+    id: '5',
     title: 'Bollywood Day/Character Day at VIT!',
     category: 'vlog',
     type: 'long',
     youtubeId: 'ELHUP5jFfoY'
   },
   {
-    id: '5',
+    id: '6',
+    title: 'Kai Cenat Edit',
+    category: 'talkingHead',
+    type: 'short',
+    youtubeId: 'ycyiH-jDo_Q'
+  },
+  {
+    id: '7',
+    title: 'Reel 1',
+    category: 'talkingHead',
+    type: 'short',
+    youtubeId: 'bDXiEe7GBUA'
+  },
+  {
+    id: '8',
     title: 'How to Bulk - 1.4M Views',
     category: 'talkingHead',
     type: 'short',
     youtubeId: '4Iw91tON3Ns'
   },
   {
-    id: '6',
+    id: '9',
     title: 'How to get Jacked - 2M Views',
     category: 'talkingHead',
     type: 'short',
     youtubeId: '6Vr-IIQa9nA'
   },
   {
-    id: '7',
+    id: '10',
     title: 'Ad 1',
     category: 'advertisement',
     type: 'short',
     youtubeId: 'ZBnk2atcdew'
   },
   {
-    id: '8',
+    id: '11',
     title: 'Ad 2',
     category: 'advertisement',
     type: 'short',
     youtubeId: 'qPZ6_dZcn90'
   },
   {
-    id: '9',
+    id: '12',
     title: 'Ad 3',
     category: 'advertisement',
     type: 'short',
     youtubeId: 'd6dkNWVVVME'
   },
-  {
-    id: '10',
-    title: 'Reel 1',
-    category: 'talkingHead',
-    type: 'short',
-    youtubeId: 'bDXiEe7GBUA'
-  },
+  
 ];
 
 export const getFeaturedVideos = (type?: 'short' | 'long') => {
